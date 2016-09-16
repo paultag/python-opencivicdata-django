c8cc67981a3a2f3780af8b35055f83cc16ca4e94
Paul Tagliamonte <tag@pault.ag>
2016-09-16 09:51:40 -0400

    fork again, remove orgs

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name="django",
+setup(name="person",
       version='0.8.2',
       author="Paul Tagliamonte",
       author_email='paultag@gmail.com',
       license="BSD",
-      description="python popolo library",
+      description="python person library",
       long_description="",
       url="",
-      py_modules=['popolo'],
+      py_modules=['person'],
       packages=[],
       include_package_data=True,
       install_requires=[
@@ -22,5 +22,6 @@ setup(name="django",
                    "Natural Language :: English",
                    "Operating System :: OS Independent",
                    "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
                    ],
       )
